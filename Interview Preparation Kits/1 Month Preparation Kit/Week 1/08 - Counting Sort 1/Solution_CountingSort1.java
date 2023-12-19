5be2d6bb65be5078c2b04d474ad5f644d24d4fb2
cfernandezcairo <cfernandezcairo@gmail.com>
2023-12-18 22:56:05 -0500

    Counting Sort

---
+++ Interview Preparation Kits/1 Month Preparation Kit/Week 1/08 - Counting Sort 1/Solution_CountingSort1.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result_CountingSort1 {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort_cfernandezcairo(List<Integer> arr) {
+        // Write your code here
+
+        List<Integer> result = Stream.iterate(0, n -> 0)
+                .limit(100)
+                .collect(Collectors.toList());
+
+        for (Integer i : arr) {
+            result.set(i, result.get(i) + 1);
+        }
+
+        return result;
+    }
+
+}
+
+public class Solution_CountingSort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result_CountingSort1.countingSort_cfernandezcairo(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
