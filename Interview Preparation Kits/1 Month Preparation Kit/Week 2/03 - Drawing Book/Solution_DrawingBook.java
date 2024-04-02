e788c2d105e56ae935cd9467749f1b5a317cf59f
Carlos Alberto Fernández Cairó <cfernandezcairo@gmail.com>
2024-04-02 13:26:40 -0400

    Week 2. 03 Drawing Book preparation

---
+++ Interview Preparation Kits/1 Month Preparation Kit/Week 2/03 - Drawing Book/Solution_DrawingBook.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result_DrawingBook {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+
+        int forwardMin = p/2;
+        int backwardMin = n/2 - forwardMin;
+
+        return  forwardMin < backwardMin ? forwardMin : backwardMin;
+
+    }
+
+}
+
+public class Solution_DrawingBook {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result_DrawingBook.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
