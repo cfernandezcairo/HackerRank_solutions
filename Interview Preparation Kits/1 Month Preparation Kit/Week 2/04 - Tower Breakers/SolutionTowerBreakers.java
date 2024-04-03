7eec39bdf6992878ff51d6072d9d6cf8bf53c2b2
Carlos Alberto Fernández Cairó <cfernandezcairo@gmail.com>
2024-04-03 11:22:37 -0400

    Week 2. 04 Tower Breakers (1)

--- Interview Preparation Kits/1 Month Preparation Kit/Week 2/04 - Tower Breakers/SolutionTowerBreakers.java
+++ Interview Preparation Kits/1 Month Preparation Kit/Week 2/04 - Tower Breakers/SolutionTowerBreakers.java
@@ -14,8 +14,7 @@ class ResultTowerBreakers {
 
     public static int towerBreakers(int n, int m) {
         // Write your code here
-
-        return 0;
+        return (n % 2 == 0 || m == 1) ? 2 : 1;
     }
 
 }
